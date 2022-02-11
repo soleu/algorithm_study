ef886531f271b026caa372eb6436017a90684017
soleu <dlthf555@gmail.com>
2022-02-11 17:27:46 +0900

    add : 1541

--- greedy/1541.py
+++ greedy/1541.py
@@ -1,15 +1,9 @@
-# 55-50+40
-import re
-
-arr = input()
-nums = list(map(int, (re.split('[+-]', arr))))
-symbol = re.split('[0-9]', arr)
-symbol = list(filter(None, symbol))
-total = nums[len(nums) - 1]
-temp = 0
-for i in range(len(symbol) - 1, -1, -1):
-    if (symbol[i] == "+"):
-        total += nums[i]
+sepNums = input().split('-')
+result = 0
+for i in range(len(sepNums)):
+    addNums = list(map(int, sepNums[i].split('+')))
+    if (i == 0):
+        result = sum(addNums)
     else:
-        total = nums[i] - total
-print(total)
+        result -= sum(addNums)
+print(result)
