b046e619a47772efb1d4f0f16c20053f310d6827
soleu <dlthf555@gmail.com>
2022-08-01 21:27:41 +0900

    add : 삽입 정렬

---
+++ Java/JAVA/src/main/java/sorting/InsertSort.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class InsertSort {
+    public String Solution(int[] arr) {
+        String answer = "";
+        for (int idx = 1; idx < arr.length; idx++) {
+            for (int i = 0; i < idx; i++) {
+                if (arr[idx] < arr[i]) {
+                    int temp = arr[idx];
+                    for (int j = idx - 1; j >= i; j--) {
+                        arr[j + 1] = arr[j];
+                    }
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        for (int x : arr) {
+            answer += x + " ";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        InsertSort m = new InsertSort();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(m.Solution(arr));
+    }
+}
